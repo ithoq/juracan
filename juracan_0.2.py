a68739e28cbf6f2665b06408fdb48240db7e842c
thay <13409083+teehay@users.noreply.github.com>
2020-09-01 15:45:01 -0700

    Update and rename juracan_0.1.py to juracan_0.2.py
    Added --sqlite flag and functionality

--- juracan_0.2.py
+++ juracan_0.2.py
@@ -6,8 +6,6 @@
 
 # Current feature priority:
 # Connect script to databases and add corresponding flags
-# Add Chromium support with flag
-# Add more output options (and flags) for use with other popular tools
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
@@ -18,14 +16,17 @@ from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
 import argparse
 import sys
+import mysql.connector #to be added
+import sqlite3
 
 if __name__ == "__main__":
 
     ## Handle args
     parser = argparse.ArgumentParser(description='Gets ASN and ISP addresses related to the target keyword from the Hurricane Electric Internet Services website. Please do not abuse this service!')
-    parser.add_argument('keyword', metavar='keyword', type=str, nargs='?', help='Target keyword')
+    parser.add_argument('keyword', metavar='keyword', type=str, nargs='?', help='target keyword')
+    parser.add_argument('--sqlite',  metavar=('dbpath', 'tablename'), nargs=2, help='store results in a SQLite database')
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         parser.print_usage()
         sys.exit(1)
     else:
@@ -40,13 +41,18 @@ if __name__ == "__main__":
     driver.get('https://bgp.he.net')
 
     ## Perform search and wait for results to populate
-    wait.until(expected.visibility_of_element_located((By.NAME, 'search[search]'))).send_keys(arg.keyword + Keys.ENTER)
+    wait.until(expected.presence_of_element_located((By.NAME, 'search[search]'))).send_keys(arg.keyword + Keys.ENTER)
     wait.until(expected.element_to_be_clickable((By.NAME, 'commit'))).click()
-    wait.until(expected.visibility_of_element_located((By.TAG_NAME, 'table')))
+    wait.until(expected.presence_of_element_located((By.TAG_NAME, 'table')))
 
     ## Set up BeautifulSoup and find individual results
     soup = BeautifulSoup(driver.page_source, features='lxml')
     results = soup.find_all('tr')
+
+    if arg.sqlite:
+        conn = sqlite3.connect(arg.sqlite[0])
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS " + arg.sqlite[1] + " (id INTEGER UNIQUE, result TEXT UNIQUE, description TEXT, country TEXT, PRIMARY KEY(id))")
 
     ## Cycle through results and print header, then results in formatted form
     for i in range(1, len(results)):
@@ -55,9 +61,18 @@ if __name__ == "__main__":
             print(f"{'Results:':<44}{'Description:':<48}{'Country:':<16}")
         else:
             data = results[i].find_all('td')
+
             res = data[0].get_text()
             desc = data[1].get_text()
             country = data[1].div.img['title']
+
             print(f"{res:<44}{desc:<48}{country:<16}")
 
+            if arg.sqlite:
+                c.execute("INSERT INTO " + arg.sqlite[1] + " (result, description, country) VALUES (?, ?, ?)", (res, desc, country))
+
+    if arg.sqlite:
+        conn.commit()
+        conn.close()
+                  
     driver.quit()
